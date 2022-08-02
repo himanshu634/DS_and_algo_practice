7df9e2e30f9938754828099faddf25affc330eca
Himashu Mendapara <himanshumendapra@gmail.com>
2022-08-02 22:27:43 +0530

    minr changes

--- Recursion/src/Main.java
+++ Recursion/src/Main.java
@@ -4,9 +4,24 @@
 import com.LectureExample.Dice;
 import com.backtracking.Maze;
 
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+//        try {
+//            int y = 10/0;
+//            System.out.println(y);
+//        }catch(ArithmeticException aex){
+//            System.out.println("arithmetic exceptin");
+//        }catch(Exception ex){
+//            System.out.println("exception");
+//        } finally {
+//            System.out.println("finally");
+//        }
 //        MergeSort sc = new MergeSort();
 //        int[] arr = {9,3, 4, 5, 6};
 //        int[] ans = sc.mergeSort(arr);
@@ -29,16 +44,46 @@ public class Main {
 //        Dice dice = new Dice();
 //        System.out.println(dice.findCombinationArrayList("",4));
 
-        Maze maze = new Maze();
-        System.out.println(maze.countPaths(3, 3));
-//        maze.printPaths2("",3, 3);
-        boolean[][] board = {{true, true, true},
-                {true, true, true},
-                {true, true, true}};
+//        Maze maze = new Maze();
+//        System.out.println(maze.countPaths(3, 3));
+////        maze.printPaths2("",3, 3);
+//        boolean[][] board = {{true, true, true},
+//                {true, true, true},
+//                {true, true, true}};
+//
+//        int[][] path = new int[board.length][board[0].length];
+//
+//        maze.allPathPrint("", board,0, 0, path, 1);
 
-        int[][] path = new int[board.length][board[0].length];
-
-        maze.allPathPrint("", board,0, 0, path, 1);
+//        triangle(new int[]{1, 6, 8});
     }
+
+//    public void printAllNonMatching(String testString){
+//        String ans = "";
+//        for(int i = 0; i < testString.length(); i++){
+//            String ch = String.valueOf(testString.charAt(i));
+//
+//            if((testString.substring(0, i)).contains(ch) || (testString.substring(i + 1).contains(ch))){
+//                continue;
+//            }
+//
+//            ans += ch;
+//        }
+//
+//        System.out.println(ans);
+//
+//
+//    }
+
+//    public void canFormTriangle(int[] arr){
+//        if((arr[0] + arr[1] < arr[2]) || (arr[1] + arr[2] < arr[0]) || (arr[0] + arr[2] < arr[1])){
+//            System.out.println("NO");
+//        }else{
+//            System.out.println("YES");
+//        }
+//
+//    }
+
+
 }
 
